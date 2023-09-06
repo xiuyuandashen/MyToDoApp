d7f9d6700e014ae1864c3870ca21987634e83591
HLT <HLT@DESKTOP-OSTD3HB>
2023-09-06 18:04:26 +0800

    feat:备忘录、待办页面设计

--- MyToDoApp/ViewModels/MemoViewModel.cs
+++ MyToDoApp/ViewModels/MemoViewModel.cs
@@ -1,6 +1,9 @@
-﻿using Prism.Mvvm;
+﻿using MyToDoApp.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,49 @@ namespace MyToDoApp.ViewModels
 {
     public class MemoViewModel : BindableBase
     {
+
+        public MemoViewModel()
+        {
+            MemoDtos = new ObservableCollection<MemoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+
+        private ObservableCollection<MemoDto> meMoDtos;
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return meMoDtos; }
+            set { meMoDtos = value; RaisePropertyChanged(); }
+        }
+
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                MemoDtos.Add(new MemoDto()
+                {
+                    Title = $"标题{i}",
+                    Content = "测试数据..."
+                });
+            }
+        }
     }
 }
--- MyToDoApp/ViewModels/TodoViewModel.cs
+++ MyToDoApp/ViewModels/TodoViewModel.cs
@@ -1,6 +1,9 @@
-﻿using Prism.Mvvm;
+﻿using MyToDoApp.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +12,50 @@ namespace MyToDoApp.ViewModels
 {
     public class TodoViewModel :BindableBase
     {
+
+        public TodoViewModel()
+        {
+            ToDoDtos =new ObservableCollection<ToDoDto> ();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+
+        
+
+        private ObservableCollection<ToDoDto> toDoDtos;
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }
+        }
+
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                ToDoDtos.Add(new ToDoDto() {
+                    Title=$"标题{i}" ,
+                    Content = "测试数据..."
+                });
+            }
+        }
     }
 }
